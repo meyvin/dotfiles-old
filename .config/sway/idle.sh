e60ad6e8732582923ab3e1a7721bc8181c1549a2
Meyvin <meyvin@mailbox.org>
2021-10-12 15:11:12 +0200

    no seperate sway configs

--- .config/sway/idle.sh
+++ .config/sway/idle.sh
@@ -2,7 +2,8 @@
 
 swayidle -w -d \
 	timeout 300 '~/.config/sway/lock.sh -f --grace 12 --fade-in 10' \
-	timeout 440 'swaymsg "output * dpms off"' \
-	resume 'swaymsg "output * dpms on"; sleep 2; swaymsg "output * enable"' \
-	before-sleep 'pgrep swaylock || ~/.config/sway/lock.sh -f --fade-in 0 && true'
+  timeout 600 'swaymsg "output * dpms off"' \
+    resume  'swaymsg "output * dpms on"'  \
+  timeout 900 'systemctl suspend ' \
+  before-sleep 'pgrep swaylock || ~/.config/sway/lock.sh -f --fade-in 0 && true'
 
