3ed10d813b47af1661554295bf21379668e4df7d
Meyvin <meyvin@mailbox.org>
2021-10-12 09:58:41 +0200

    fix

--- .config/sway/idle.sh
+++ .config/sway/idle.sh
@@ -2,7 +2,7 @@
 
 swayidle -w -d \
 	timeout 300 '~/.config/sway/lock.sh -f --grace 12 --fade-in 10' \
-	timeout 140 'swaymsg "output * dpms off"' \
+	timeout 440 'swaymsg "output * dpms off"' \
 	resume 'swaymsg "output * dpms on"; sleep 2; swaymsg "output * enable"' \
 	before-sleep 'pgrep swaylock || ~/.config/sway/lock.sh -f --fade-in 0 && true'
 
