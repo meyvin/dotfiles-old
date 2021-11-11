c1fbd672896415005679bad172d4a3e2405c450d
Meyvin <meyvin@mailbox.org>
2021-11-11 17:26:31 +0100

    added gammastep

--- .config/sway/idle.sh
+++ .config/sway/idle.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 swayidle -w -d \
-	timeout 300 '~/.config/sway/lock.sh -f --grace 12 --fade-in 10' \
+	timeout 300 '~/.config/sway/lock.sh -f --grace 12 --fade-in 4' \
 	timeout 600 'swaymsg "output * dpms off"' \
   timeout 630 'systemctl suspend' \
 	resume 'swaymsg "output * dpms on"; sleep 2; swaymsg "output * enable"' \
