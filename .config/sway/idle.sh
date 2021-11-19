ec8f0f4fd6ef2fc56267a5108b1339d2f15922c1
Meyvin <meyvin@mailbox.org>
2021-11-19 13:31:54 +0100

    changed idle time

--- .config/sway/idle.sh
+++ .config/sway/idle.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 swayidle -w -d \
-	timeout 300 '~/.config/sway/lock.sh -f --grace 12 --fade-in 4' \
-	timeout 600 'swaymsg "output * dpms off"' \
-  timeout 630 'systemctl suspend' \
+	timeout 600 '~/.config/sway/lock.sh -f --grace 12 --fade-in 4' \
+	timeout 900 'swaymsg "output * dpms off"' \
+  timeout 930 'systemctl suspend' \
 	resume 'swaymsg "output * dpms on"; sleep 2; swaymsg "output * enable"' \
 	before-sleep 'pgrep swaylock || ~/.config/sway/lock.sh -f --fade-in 0 && true'
